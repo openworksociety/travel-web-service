fbd31b473cff5516c6403799596ee5fcffc4aa9d
hkanure <hkanure@PU-NB-HKEN.na.rtdom.net>
2021-02-18 01:29:24 +0530

    TA-9 : added JPA, Hibernate Demo Implementation

--- travel-service-api/src/main/java/com/travel/service/api/SampleTravelService.java
+++ travel-service-api/src/main/java/com/travel/service/api/SampleTravelService.java
@@ -1,18 +1,39 @@
 package com.travel.service.api;
 
+import java.util.List;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import com.travel.service.dto.VehicleDto;
 
-
+/**
+ * @author hkanure
+ *
+ *         USE:
+ */
 @Path("sample")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public interface SampleTravelService {
 
-	@GET
-	@Path("/testmsg")
-	public String testMsg();
+  @GET
+  @Path("/testmsg")
+  String testMsg();
+
+  @GET
+  @Path("/findAllVehicles")
+  List<VehicleDto> findAllVehicles();
+
+  @DELETE
+  @Path("/deletebyId/{id}")
+  void deleteById(@PathParam("id") Long id);
+
+  @POST
+  @Path("/create")
+  void create(VehicleDto dto);
 }
--- travel-service-impl/src/main/java/com/travel/service/config/SpringConfiguration.java
+++ travel-service-impl/src/main/java/com/travel/service/config/SpringConfiguration.java
@@ -1,13 +1,12 @@
 package com.travel.service.config;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "com.travel.service.*")
-@EnableAutoConfiguration(exclude = DataSourceAutoConfiguration.class)
+@EnableAutoConfiguration
 public class SpringConfiguration {
 
 }
